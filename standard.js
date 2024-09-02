8c0aa2d0e9709fc903bc80f5ba8c2e9aa09dece2
leo sun <xsun051@hotmail.com>
2024-09-02 17:34:21 +1200

    tes

--- standard.js
+++ standard.js
@@ -59,7 +59,7 @@ class SpaceIndicator {
             .button-container {
                 position: fixed;
                 top: 20px;
-                left: 20px;
+                right: 20px;
                 display: flex;
                 flex-direction: column;
                 z-index: 99999;
